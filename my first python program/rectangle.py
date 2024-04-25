8ba90885268d299456a14464a4d59815a2b1af2a
kingfestus <168098548+kingfestus@users.noreply.github.com>
2024-04-25 14:17:17 +0100

    rectangle.py

---
+++ my first python program/rectangle.py
@@ -0,0 +1,5 @@
+'This program will calculate the area of a rectangle '
+length = int(input("Pls enter your length $"))
+width = int(input(" Pls enter your width $"))
+area = length * width
+print(f'the area of your rectangle is {area}')
