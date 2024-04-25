2ca4fa342b2c5466586bfc9ddbbefb829ba4e6c8
kingfestus <168098548+kingfestus@users.noreply.github.com>
2024-04-25 14:18:00 +0100

    Add files via upload

---
+++ my first python program/bodmas.py
@@ -0,0 +1,9 @@
+' This program will exercise the bodmas application'
+num1 = int(input(" Pls enter your first number"))
+num2 = int(input(" Pls enter your second number"))
+addition= print( f' the addition of {num1} and {num2} is {num1 + num2}')
+difference = print( f' the difference of {num1} and {num2} is {num1 - num2}')
+product = print( f' the product of {num1} and {num2} is {num1 * num2}')
+quotient= print( f' the quotient of {num1} and {num2} is {num1 / num2}')
+
+
---
+++ my first python program/tempconvert.py
@@ -0,0 +1,4 @@
+'This program will convert the temperature from celsius to fahrenheit'
+celcius = int(input("Pls enter your temperature"))
+fahrenheit = (celcius *9/5+32)
+print(f'your celcius has been converted from {celcius} to {fahrenheit}')
