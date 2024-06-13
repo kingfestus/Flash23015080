20f5fad16152901bcccdec47e21c642bd89098d3
kingfestus <168098548+kingfestus@users.noreply.github.com>
2024-06-13 11:14:48 +0100

    emailvalidator.py

--- Regex/emailvalidator.py
+++ Regex/emailvalidator.py
@@ -1,10 +1,7 @@
 import re
-mail = input("Enter your email address: ")
-    # Regular expression for validating email addresses
-mail_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-
-# Check if the email matches the pattern
-if re.match(mail_regex, mail):
-     print("valid email")
+user = input("Enter your email address: ")
+regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+if re.match(regex, user):
+    print("valid email")
 else:
     print('not matched to regular email')
